18be224e41181db4c459b63f09dd4c8f9946d85a
arkajenar07 <jenararka019@gmail.com>
2023-03-11 10:30:41 +0700

    commit maret

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>GAMIFY</title>
 </head>
 <body>
     <nav>
@@ -12,7 +13,7 @@
             <li>
                 <a href="#">
                     <span class="icon">
-                        <ion-icon name="settings-outline"></ion-icon>
+                        
                     </span>
                     <span class="txt">SETTINGS</span>
                 </a>
@@ -38,7 +39,7 @@
             <li>
                 <a href="#">
                     <span class="icon"></span>
-                    <span class="txt"></span>
+                    <span class="txt">NONE</span>
                 </a>
             </li>
             <li>
@@ -50,12 +51,11 @@
             <li>
                 <a href="#">
                     <span class="icon"></span>
-                    <span class="txt">HOME</span>
+                    <span class="txt">MUSIC</span>
                 </a>
             </li>
         </ul>
     </nav>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
 </body>
 </html>
